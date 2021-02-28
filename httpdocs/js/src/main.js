fbba0ff3deec255db0150ed160c8ae915dcc4923
olliefw <ollie@blackbridge.co.uk>
2021-02-28 17:10:27 +0000

    Add IE forEach polyfill

--- httpdocs/js/src/main.js
+++ httpdocs/js/src/main.js
@@ -2,6 +2,14 @@
 import interact from 'interactjs';
 import arc from './modules/arc.js';
 
+// forEach polyfill for IE
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
+}
+if (window.HTMLCollection && !HTMLCollection.prototype.forEach) {
+	HTMLCollection.prototype.forEach = Array.prototype.forEach;
+}
+
 
 arc();
 
