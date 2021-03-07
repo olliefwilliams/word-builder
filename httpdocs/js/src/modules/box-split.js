31204c4d680092c85d76e34e5737932d3e33d25c
olliefw <ollie@blackbridge.co.uk>
2021-03-07 20:52:16 +0000

    Added box splitting functionality, tweaked css buttons

--- httpdocs/js/src/modules/box-split.js
+++ httpdocs/js/src/modules/box-split.js
@@ -12,6 +12,26 @@ export default function () {
 			element.classList.remove("active");
 		});
 		this.classList.add("active");
+
+		let canvas = document.querySelector(".canvas");
+
+		let block = document.createElement("div");
+		block.classList.add("canvas__block");
+
+		let fragment = new DocumentFragment();
+		// remove all
+		while (canvas.firstChild) {
+			canvas.removeChild(canvas.firstChild);
+		}
+
+		for (let i = 0; i < number; i++) {
+			let blockClone = block.cloneNode();
+			fragment.appendChild(blockClone);
+		}
+
+		canvas.appendChild(fragment);
+
+
 	}
 
 }
